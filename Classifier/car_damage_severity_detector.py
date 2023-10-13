b2c7e68e5a0bdf080debbb2a099880f9f996acb8
ciganoliviu <ciganoliviudavid@gmail.com>
2023-10-14 01:41:47 +0300

    Update with classifier

---
+++ Classifier/car_damage_severity_detector.py
@@ -0,0 +1,24 @@
+import os
+
+from django.core.cache import cache
+import cv2
+import joblib
+
+
+def car_damage_severity_detector(image_source):
+    classifier_classes = {0: 'minor', 1: 'moderate', 2: 'severe'}
+    model_cache_key = 'model_cache'
+    model = cache.get(model_cache_key)
+    model_rel_path = 'Classifier/model_cache/cache.pkl'
+
+    if model is None:
+        model_path = os.path.realpath(model_rel_path)
+        model = joblib.load(model_path)
+        cache.set(model_cache_key, model, None)
+
+    img = cv2.imread(image_source, 0)
+    img_resized = cv2.resize(img, (450, 450))
+    img_resized = img_resized.reshape(1, -1) / 255
+    path = model.predict(img_resized)
+
+    return classifier_classes[path[0]]

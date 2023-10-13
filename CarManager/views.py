af85af30231a7cd27e0efcb216150c8a89d91678
ciganoliviu <ciganoliviudavid@gmail.com>
2023-10-13 23:04:39 +0300

    Updates

--- CarManager/views.py
+++ CarManager/views.py
@@ -1,3 +1,26 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
 
-# Create your views here.
+from CarManager.models import CarsReport
+from CarManager.serializer import CarsReportSerializer
+
+
+class CategorieRestInterface(APIView):
+    @staticmethod
+    def get(request):
+        data_objects = CarsReport.objects.all()
+        serializer = CarsReportSerializer(data_objects, many=True)
+
+        return Response(serializer.data)
+
+    @staticmethod
+    def post(request):
+        serializer = CarsReportSerializer(data=request.data, many=True)
+
+        if serializer.is_valid():
+            serializer.save()
+            return Response(data=serializer.data, status=status.HTTP_201_CREATED)
+
+        return Response(data=serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+

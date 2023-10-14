f62dd9a0bda7d65945ab59e811890efae0e8b8c3
ciganoliviu <ciganoliviudavid@gmail.com>
2023-10-14 15:57:30 +0300

    Updates

--- Classifier/dependencies.py
+++ Classifier/dependencies.py
@@ -4,6 +4,7 @@ import os
 
 STANDARD_DATA_DIRECTORY_STRUCTURE = 'dataset/validation/'
 STANDARD_RESIZE_VALUE = 300
+classifier_classes = {'minor': 0, 'moderate': 1, 'severe': 2}
 
 
 def collect_images_from_files(X, Y, classifier_classes, resize_value=STANDARD_RESIZE_VALUE):
@@ -16,17 +17,16 @@ def collect_images_from_files(X, Y, classifier_classes, resize_value=STANDARD_RE
             Y.append(classifier_classes[cls_class])
 
 
-def test_based_on_images(classifier, fileTestingClass, size=9, resize_value=STANDARD_RESIZE_VALUE):
+def test_based_on_images(classifier, file_testing_class, size=9, resize_value=STANDARD_RESIZE_VALUE):
     values_classifier = {0: 'minor', 1: 'moderate', 2: 'severe'}
 
     plt.figure(figsize=(12, 8))
-
     subplot_index = 1
 
-    for i in os.listdir(STANDARD_DATA_DIRECTORY_STRUCTURE + fileTestingClass)[:size]:
+    for i in os.listdir(STANDARD_DATA_DIRECTORY_STRUCTURE + file_testing_class)[:size]:
         plt.subplot(3, 3, subplot_index)
 
-        img = cv2.imread(STANDARD_DATA_DIRECTORY_STRUCTURE + fileTestingClass + i, 0)
+        img = cv2.imread(STANDARD_DATA_DIRECTORY_STRUCTURE + file_testing_class + i, 0)
         img_resized = cv2.resize(img, (resize_value, resize_value))
         img_resized = img_resized.reshape(1, -1) / 255
         path = classifier.predict(img_resized)
@@ -40,11 +40,11 @@ def test_based_on_images(classifier, fileTestingClass, size=9, resize_value=STAN
             subplot_index += 1
 
 
-def scale_data_tests(xTrain, xTest):
-    xTrain = xTrain / 255
-    xTest = xTest / 255
+def scale_data_tests(x_train, x_test):
+    x_train = x_train / 255
+    x_test = x_test / 255
 
-    return xTrain, xTest
+    return x_train, x_test
 
 
 def run_live_images_testing(classifier, image_show_series, new_image_resize_value):

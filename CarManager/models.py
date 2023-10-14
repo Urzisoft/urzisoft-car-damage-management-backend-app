c65e040a860da1bae94cddc7595701a8903589ac
ciganoliviu <ciganoliviudavid@gmail.com>
2023-10-14 16:21:50 +0300

    Updates

--- CarManager/models.py
+++ CarManager/models.py
@@ -2,11 +2,11 @@ from django.db import models
 
 
 class CarsReport(models.Model):
-    license_plate = models.CharField(max_length=15, default='X')
+    license_plate = models.CharField(max_length=15, default='X', unique=True)
     image_url = models.ImageField(upload_to='car-manager/data-to-identify')
     damage_severity = models.CharField(max_length=15, blank=True)
     car_entry_date = models.DateTimeField(auto_now_add=True)
-    car_leave_date = models.DateTimeField(blank=True)
+    car_leave_date = models.DateTimeField(blank=True, null=True)
     done = models.BooleanField(default=False)
 
     def __str__(self):

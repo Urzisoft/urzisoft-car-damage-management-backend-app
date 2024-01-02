c27ebbc77f6a5269e551c5805dad3a21aa28c330
CiganOliviu <ciganoliviudavid@gmail.com>
2024-01-02 18:22:56 +0200

    Updates

--- CarManager/views.py
+++ CarManager/views.py
@@ -21,11 +21,16 @@ class CarsReportRestInterface(APIView):
     def get(request):
         damage_dispatcher = request.GET.get('damage_severity')
         service_done_dispatcher = request.GET.get('is_car_service_done')
+        field_to_order_by = request.GET.get('field_to_order_by')
+        element_to_order_by = request.GET.get('element_to_order_by')
 
         if damage_dispatcher:
             data_objects = CarsReport.objects.filter(damage_severity=damage_dispatcher)
         elif service_done_dispatcher:
             data_objects = CarsReport.objects.filter(done=service_done_dispatcher)
+        elif field_to_order_by and element_to_order_by:
+            field_type = '-' + field_to_order_by if element_to_order_by == 'newest' else field_to_order_by
+            data_objects = CarsReport.objects.order_by(field_type)
         else:
             data_objects = CarsReport.objects.all()
 
